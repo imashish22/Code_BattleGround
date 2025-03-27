36a7bdef2215cd2d3f44415297fa91accbfe8a38
imashish22 <imjhaashish22@gmail.com>
2025-03-28 01:41:49 +0530

    fnjkfnferf

--- teacher/teacher-panel/vite.config.js
+++ teacher/teacher-panel/vite.config.js
@@ -7,10 +7,4 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      // '/api': 'https://api.code-battleground.site',  // Proxy all requests starting with /api to the backend
-      '/api': 'https://teacherapi.code-battleground.site',  // Proxy all requests starting with /api to the backend
-    },
-
-}})
+})
