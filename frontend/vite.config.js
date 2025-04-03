4bb05ae9e43c016e98040a0d8a8e21d0710a3266
Ashish <110475396+imashish22@users.noreply.github.com>
2025-04-03 12:19:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://api.code-battleground.site',  // Proxy all requests starting with /api to the backend
+      // '/api': 'https://api.code-battleground.site',  
+      '/api':"https://code-battleground.onrender.com"
     },
   },
 })
